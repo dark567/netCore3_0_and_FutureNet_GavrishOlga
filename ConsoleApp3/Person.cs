a45716db0f22ceb825c8a7ee121ef074efb499a2
dark <dark@DESKTOP-F0STRUP>
2020-03-18 20:02:49 +0200

    20200318

---
+++ ConsoleApp3/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string name, string lastname)
+        {
+            this.FirstName = name;
+            this.LastName = lastname;
+        }
+
+        public Person(string name, string middleName, string lastName)
+        {
+            this.FirstName = name;
+            this.MiddleName = middleName;
+            this.LastName = lastName;
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+#nullable disable
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sheldon = new Person("Sheldon", "Cooper");
+            var lenght = GetMiddleNameLenght(sheldon);
+            Console.WriteLine(lenght);
+            Console.ReadLine();
+        }
+
+        static int GetMiddleNameLenght(Person person)
+        {
+            var middle = person.MiddleName;
+            //if (middle is null) return 0; //ver 1
+            if (string.IsNullOrEmpty(middle)) return 0; //ver 2
+            return middle.Length;
+        }
+    }
+}
